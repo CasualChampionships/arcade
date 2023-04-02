9d2e5744bc5955bf9f419d3ea5fce728700aba93
senseiwells <wellsm488@gmail.com>
2023-04-02 01:16:06 +0100

    Fix advancement handler

--- src/main/java/net/casualuhc/arcade/mixin/advancements/ServerAdvancementManagerMixin.java
+++ src/main/java/net/casualuhc/arcade/mixin/advancements/ServerAdvancementManagerMixin.java
@@ -5,6 +5,7 @@ import net.casualuhc.arcade.advancements.AdvancementHandler;
 import net.casualuhc.arcade.advancements.MutableAdvancements;
 import net.minecraft.advancements.Advancement;
 import net.minecraft.advancements.AdvancementList;
+import net.minecraft.advancements.TreeNodePosition;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.ServerAdvancementManager;
 import net.minecraft.server.packs.resources.ResourceManager;
@@ -12,6 +13,7 @@ import net.minecraft.util.profiling.ProfilerFiller;
 import org.jetbrains.annotations.NotNull;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -24,7 +26,11 @@ public class ServerAdvancementManagerMixin implements MutableAdvancements {
 
 	@Inject(
 		method = "apply(Ljava/util/Map;Lnet/minecraft/server/packs/resources/ResourceManager;Lnet/minecraft/util/profiling/ProfilerFiller;)V",
-		at = @At("TAIL")
+		at = @At(
+			value = "INVOKE",
+			target = "Lnet/minecraft/advancements/AdvancementList;getRoots()Ljava/lang/Iterable;",
+			shift = At.Shift.BEFORE
+		)
 	)
 	private void onAdvancements(Map<ResourceLocation, JsonElement> object, ResourceManager resourceManager, ProfilerFiller profiler, CallbackInfo ci) {
 		AdvancementHandler.forEachCustom(this::addAdvancement);
@@ -33,10 +39,20 @@ public class ServerAdvancementManagerMixin implements MutableAdvancements {
 	@Override
 	public void addAdvancement(@NotNull Advancement advancement) {
 		((MutableAdvancements) this.advancements).addAdvancement(advancement);
+
+		Advancement root = advancement.getRoot();
+		if (root.getDisplay() != null) {
+			TreeNodePosition.run(root);
+		}
 	}
 
 	@Override
 	public void removeAdvancement(@NotNull Advancement advancement) {
 		((MutableAdvancements) this.advancements).removeAdvancement(advancement);
+
+		Advancement root = advancement.getRoot();
+		if (root.getDisplay() != null) {
+			TreeNodePosition.run(root);
+		}
 	}
 }
