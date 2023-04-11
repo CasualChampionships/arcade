7a87c30dc0730f53731e14fc11f424a8478eff8d
senseiwells <wellsm488@gmail.com>
2023-04-11 14:52:13 +0100

    Moved task scheduler

--- src/main/kotlin/net/casualuhc/arcade/scheduler/Scheduler.kt
+++ src/main/kotlin/net/casualuhc/arcade/scheduler/Scheduler.kt
@@ -1,55 +1,29 @@
 package net.casualuhc.arcade.scheduler
 
-import it.unimi.dsi.fastutil.ints.Int2ObjectOpenHashMap
 import net.casualuhc.arcade.events.EventHandler
 import net.casualuhc.arcade.events.server.ServerTickEvent
-import java.util.*
-import java.util.function.IntFunction
+import net.casualuhc.arcade.scheduler.MinecraftTimeUnit.Ticks
 
 @Suppress("unused")
-object Scheduler: EventListener {
-    private val tasks = Int2ObjectOpenHashMap<Queue<Task>>()
-    private var ticks = 0
+object Scheduler {
+    private val impl = TaskScheduler()
 
     init {
-        EventHandler.register<ServerTickEvent> {
-            val queue = this.tasks.remove(this.ticks++)
-            if (queue !== null) {
-                queue.forEach(Task::run)
-                queue.clear()
-            }
-        }
+        EventHandler.register<ServerTickEvent> { this.impl.tick() }
     }
 
     @JvmStatic
-    fun schedule(time: Int, unit: MinecraftTimeUnit = MinecraftTimeUnit.Ticks, task: Runnable): Task {
-        return this.schedule(time, unit, Task.of(task))
+    fun schedule(time: Int, unit: MinecraftTimeUnit = Ticks, task: Runnable): Task {
+        return this.impl.schedule(time, unit, task)
     }
 
     @JvmStatic
     fun schedule(time: Int, unit: MinecraftTimeUnit, task: Task): Task {
-        require(time >= 0) { "Cannot schedule a task in the past!" }
-        this.tasks.computeIfAbsent(this.ticks + unit.toTicks(time), IntFunction { ArrayDeque() }).add(task)
-        return task
+        return this.impl.schedule(time, unit, task)
     }
 
     @JvmStatic
-    fun scheduleInLoop(
-        delay: Int,
-        interval: Int,
-        duration: Int,
-        unit: MinecraftTimeUnit = MinecraftTimeUnit.Ticks,
-        block: Runnable
-    ): Task {
-        require(delay >= 0 && interval > 0 && duration >= 0) {
-            "Delay, interval or duration ticks cannot be negative"
-        }
-        val task = Task.of(block)
-        var tick = delay
-        while (tick < duration + delay) {
-            this.schedule(tick, unit, task)
-            tick += interval
-        }
-        return task
+    fun scheduleInLoop(delay: Int, interval: Int, duration: Int, unit: MinecraftTimeUnit = Ticks, block: Runnable): Task {
+        return this.impl.scheduleInLoop(delay, interval, duration, unit, block)
     }
 }
