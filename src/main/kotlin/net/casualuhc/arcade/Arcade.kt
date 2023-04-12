2a888a4398159066fa1d6ec7812bf032683f1677
senseiwells <wellsm488@gmail.com>
2023-04-12 22:06:27 +0100

    Optimise imports

--- src/main/kotlin/net/casualuhc/arcade/Arcade.kt
+++ src/main/kotlin/net/casualuhc/arcade/Arcade.kt
@@ -1,19 +1,9 @@
 package net.casualuhc.arcade
 
-import net.casualuhc.arcade.commands.EnumArgument
 import net.casualuhc.arcade.events.EventHandler
-import net.casualuhc.arcade.events.level.LevelCreatedEvent
 import net.casualuhc.arcade.events.server.ServerCreatedEvent
-import net.casualuhc.arcade.events.server.ServerRegisterCommandEvent
-import net.casualuhc.arcade.extensions.Extension
-import net.casualuhc.arcade.utils.LevelUtils.addExtension
 import net.fabricmc.api.ModInitializer
-import net.minecraft.commands.Commands
-import net.minecraft.commands.synchronization.SuggestionProviders
-import net.minecraft.nbt.CompoundTag
-import net.minecraft.nbt.Tag
 import net.minecraft.server.MinecraftServer
-import net.minecraft.world.level.GameType
 import org.apache.logging.log4j.LogManager
 import org.apache.logging.log4j.Logger
 
