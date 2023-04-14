bf102c35e3166faf31df4aec1f0166bc9d14f18a
senseiwells <wellsm488@gmail.com>
2023-04-14 17:08:37 +0100

    Add overload

--- src/main/kotlin/net/casualuhc/arcade/resources/LocalResourcePackHost.kt
+++ src/main/kotlin/net/casualuhc/arcade/resources/LocalResourcePackHost.kt
@@ -4,8 +4,9 @@ import java.nio.file.Path
 import kotlin.io.path.listDirectoryEntries
 
 class LocalResourcePackHost(
-    private val packDirectory: Path
-): ResourcePackHost() {
+    private val packDirectory: Path,
+    threads: Int = 3
+): ResourcePackHost(threads) {
     override fun getPacks(): Iterable<ReadablePack> {
         return this.packDirectory.listDirectoryEntries("*.zip").map { ReadablePathPack(it) }
     }
