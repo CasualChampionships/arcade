09f91b203caef9110685c0d5f807a85c0ba39f98
senseiwells <wellsm488@gmail.com>
2023-04-16 11:03:11 +0100

    Make fun interface

--- src/main/kotlin/net/casualuhc/arcade/scoreboards/SidebarRow.kt
+++ src/main/kotlin/net/casualuhc/arcade/scoreboards/SidebarRow.kt
@@ -3,6 +3,6 @@ package net.casualuhc.arcade.scoreboards
 import net.minecraft.network.chat.Component
 import net.minecraft.server.level.ServerPlayer
 
-interface SidebarRow {
+fun interface SidebarRow {
     fun getComponent(player: ServerPlayer): Component
 }
