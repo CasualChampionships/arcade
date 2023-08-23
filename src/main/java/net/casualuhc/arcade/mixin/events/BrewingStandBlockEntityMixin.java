90802268fe34cad35d3e7b3c10926b4d147a03ef
senseiwells <wellsm488@gmail.com>
2023-08-23 01:00:01 +0100

    Add brewing stand event

---
+++ src/main/java/net/casualuhc/arcade/mixin/events/BrewingStandBlockEntityMixin.java
@@ -0,0 +1,41 @@
+package net.casualuhc.arcade.mixin.events;
+
+import net.casualuhc.arcade.events.GlobalEventHandler;
+import net.casualuhc.arcade.events.block.BrewingStandBrewEvent;
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.entity.BrewingStandBlockEntity;
+import net.minecraft.world.level.block.state.BlockState;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+@Mixin(BrewingStandBlockEntity.class)
+public class BrewingStandBlockEntityMixin {
+	@ModifyVariable(
+		method = "serverTick",
+		ordinal = 0,
+		at = @At(
+			value = "LOAD",
+			ordinal = 2
+		)
+	)
+	private static boolean onBrew(
+		boolean value,
+		Level level,
+		BlockPos pos,
+		BlockState state,
+		BrewingStandBlockEntity entity
+	) {
+		if (!value) {
+			return false;
+		}
+		if (((BrewingStandBlockEntityAccessor) entity).fuel() <= 0) {
+			return false;
+		}
+		BrewingStandBrewEvent event = new BrewingStandBrewEvent((ServerLevel) level, pos, state, entity);
+		GlobalEventHandler.broadcast(event);
+		return !event.isCancelled();
+	}
+}
---
+++ src/main/java/net/casualuhc/arcade/mixin/screen/ServerGamePacketListenerImplMixin.java
@@ -0,0 +1,25 @@
+package net.casualuhc.arcade.mixin.screen;
+
+import net.casualuhc.arcade.screen.InterfaceScreen;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.server.network.ServerGamePacketListenerImpl;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(ServerGamePacketListenerImpl.class)
+public class ServerGamePacketListenerImplMixin {
+	@Shadow public ServerPlayer player;
+
+	@Redirect(
+		method = "handleContainerClick",
+		at = @At(
+			value = "INVOKE",
+			target = "Lnet/minecraft/server/level/ServerPlayer;isSpectator()Z"
+		)
+	)
+	private boolean canClick(ServerPlayer instance) {
+		return !(instance.containerMenu instanceof InterfaceScreen) && instance.isSpectator();
+	}
+}
