12aea2c6f041c01628f0138dbfe5ac5dd3298680
senseiwells <wellsm488@gmail.com>
2023-08-26 15:11:46 +0100

    Make bossbar tasks cancellable

--- src/main/kotlin/net/casual/arcade/gui/bossbar/BossBarTask.kt
+++ src/main/kotlin/net/casual/arcade/gui/bossbar/BossBarTask.kt
@@ -1,17 +1,24 @@
 package net.casual.arcade.gui.bossbar
 
 import net.casual.arcade.minigame.Minigame
-import net.casual.arcade.scheduler.Task
+import net.casual.arcade.scheduler.CancellableTask
 
 abstract class BossBarTask(
     val owner: Minigame,
     private val bar: CustomBossBar
-): Task {
+): CancellableTask {
+    private var cancel = false
+
     init {
         this.owner.addBossbar(this.bar)
     }
 
+    final override fun isCancelled(): Boolean {
+        return this.cancel
+    }
+
     override fun run() {
         this.owner.removeBossbar(this.bar)
+        this.cancel = true
     }
 }
