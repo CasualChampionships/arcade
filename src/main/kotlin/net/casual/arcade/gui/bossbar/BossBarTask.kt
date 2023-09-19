cdfdc21b749597abb2efc879b1fd6e677daff0ea
senseiwells <wellsm488@gmail.com>
2023-09-19 23:35:13 +0100

    Re-add BossBarTask

---
+++ src/main/kotlin/net/casual/arcade/gui/bossbar/BossBarTask.kt
@@ -0,0 +1,24 @@
+package net.casual.arcade.gui.bossbar
+
+import net.casual.arcade.minigame.Minigame
+import net.casual.arcade.task.CancellableTask
+
+public abstract class BossBarTask(
+    public val owner: Minigame<*>,
+    private val bar: CustomBossBar
+): CancellableTask {
+    private var cancel = false
+
+    init {
+        this.owner.addBossbar(this.bar)
+    }
+
+    final override fun isCancelled(): Boolean {
+        return this.cancel
+    }
+
+    override fun run() {
+        this.owner.removeBossbar(this.bar)
+        this.cancel = true
+    }
+}
