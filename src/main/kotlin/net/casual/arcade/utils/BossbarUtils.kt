e322d1547138b4a85d20e1c5805b5c4512ff7f48
senseiwells <wellsm488@gmail.com>
2023-10-15 18:29:07 +0100

    Add withDuration extension method

--- src/main/kotlin/net/casual/arcade/utils/BossbarUtils.kt
+++ src/main/kotlin/net/casual/arcade/utils/BossbarUtils.kt
@@ -4,14 +4,23 @@ import net.casual.arcade.events.GlobalEventHandler
 import net.casual.arcade.events.player.PlayerCreatedEvent
 import net.casual.arcade.events.player.PlayerLeaveEvent
 import net.casual.arcade.events.player.PlayerTickEvent
+import net.casual.arcade.gui.bossbar.TimerBossBar
 import net.casual.arcade.gui.extensions.PlayerBossbarsExtension
+import net.casual.arcade.gui.task.BossBarTask
+import net.casual.arcade.scheduler.MinecraftTimeDuration
 import net.casual.arcade.utils.PlayerUtils.addExtension
 import net.casual.arcade.utils.PlayerUtils.getExtension
 import net.minecraft.server.level.ServerPlayer
 
-internal object BossbarUtils {
+public object BossbarUtils {
     internal val ServerPlayer.bossbars
         get() = this.getExtension(PlayerBossbarsExtension::class.java)
+
+
+    public fun BossBarTask<TimerBossBar>.withDuration(duration: MinecraftTimeDuration): BossBarTask<TimerBossBar> {
+        this.bar.setDuration(duration)
+        return this
+    }
 
     internal fun registerEvents() {
         GlobalEventHandler.register<PlayerCreatedEvent> { (player) ->
