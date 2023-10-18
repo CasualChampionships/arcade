65925aa5f83fe61beff08f3e39f67c4889e0c66c
senseiwells <wellsm488@gmail.com>
2023-10-19 00:38:56 +0100

    Fix PlayerAttackEvent

--- src/main/java/net/casual/arcade/mixin/events/PlayerMixin.java
+++ src/main/java/net/casual/arcade/mixin/events/PlayerMixin.java
@@ -9,30 +9,45 @@ import net.casual.arcade.events.player.PlayerDamageEvent;
 import net.casual.arcade.utils.CastUtils;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(Player.class)
 public class PlayerMixin {
-	@WrapWithCondition(
+	@Redirect(
 		method = "attack",
 		at = @At(
 			value = "INVOKE",
 			target = "Lnet/minecraft/world/entity/LivingEntity;hurt(Lnet/minecraft/world/damagesource/DamageSource;F)Z"
 		)
 	)
-	private boolean onAttack(LivingEntity entity, DamageSource source, float amount) {
+	private boolean onSweepAttack(LivingEntity entity, DamageSource source, float amount) {
+		return this.onAttack(entity, source, amount);
+	}
+
+	@Redirect(
+		method = "attack",
+		at = @At(
+			value = "INVOKE",
+			target = "Lnet/minecraft/world/entity/Entity;hurt(Lnet/minecraft/world/damagesource/DamageSource;F)Z"
+		)
+	)
+	private boolean onAttack(Entity entity, DamageSource source, float amount) {
 		ServerPlayer player = CastUtils.tryCast(ServerPlayer.class, this);
-		if (player == null) {
-			return false;
+		if (player != null) {
+			PlayerAttackEvent event = new PlayerAttackEvent(player, entity, amount);
+			GlobalEventHandler.broadcast(event);
+			if (event.isCancelled()) {
+				return false;
+			}
 		}
-		PlayerAttackEvent event = new PlayerAttackEvent(player, entity, amount);
-		GlobalEventHandler.broadcast(event);
-		return !event.isCancelled();
+		return entity.hurt(source, amount);
 	}
 
 	@Inject(
