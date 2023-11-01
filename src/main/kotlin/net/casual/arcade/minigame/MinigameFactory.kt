777bdde34ee5c753f668a93b6e923ab09a0d778c
senseiwells <wellsm488@gmail.com>
2023-11-01 02:04:54 +0000

    Make MinigameFactory a functional interface

--- src/main/kotlin/net/casual/arcade/minigame/MinigameFactory.kt
+++ src/main/kotlin/net/casual/arcade/minigame/MinigameFactory.kt
@@ -2,6 +2,6 @@ package net.casual.arcade.minigame
 
 import net.minecraft.server.MinecraftServer
 
-public interface MinigameFactory {
+public fun interface MinigameFactory {
     public fun create(server: MinecraftServer): Minigame<*>
 }
