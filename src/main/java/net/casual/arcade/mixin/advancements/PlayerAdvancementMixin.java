6349f16ee379df3579560b5fd95afede75b1923a
senseiwells <wellsm488@gmail.com>
2023-11-22 23:55:43 +0000

    Shush logger

---
+++ src/main/java/net/casual/arcade/mixin/advancements/PlayerAdvancementMixin.java
@@ -0,0 +1,21 @@
+package net.casual.arcade.mixin.advancements;
+
+import com.llamalad7.mixinextras.injector.WrapWithCondition;
+import net.minecraft.server.PlayerAdvancements;
+import org.slf4j.Logger;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(PlayerAdvancements.class)
+public class PlayerAdvancementMixin {
+	@WrapWithCondition(
+		method = "method_48026",
+		at = @At(
+			value = "INVOKE",
+			target = "Lorg/slf4j/Logger;warn(Ljava/lang/String;Ljava/lang/Object;Ljava/lang/Object;)V"
+		)
+	)
+	private boolean onWarnNoAdvancement(Logger instance, String string, Object o1, Object o2) {
+		return false;
+	}
+}
