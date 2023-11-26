29c02ce8d673a654ef401003a0a34daf9efc9cb1
senseiwells <wellsm488@gmail.com>
2023-11-26 20:32:53 +0000

    Add random ResourceLocation

---
+++ src/main/kotlin/net/casual/arcade/utils/ResourceUtils.kt
@@ -0,0 +1,12 @@
+package net.casual.arcade.utils
+
+import net.casual.arcade.Arcade
+import net.minecraft.resources.ResourceLocation
+import org.apache.commons.lang3.RandomStringUtils
+
+public object ResourceUtils {
+    public fun random(): ResourceLocation {
+        val key = RandomStringUtils.random(16, "abcdefghijklmnopqrstuvwxyz0123456789")
+        return ResourceLocation(Arcade.MOD_ID, key)
+    }
+}
