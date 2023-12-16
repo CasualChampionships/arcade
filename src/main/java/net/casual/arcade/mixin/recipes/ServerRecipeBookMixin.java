c0c6158d0c68b218022680781cd05e2347f3c107
senseiwells <wellsm488@gmail.com>
2023-12-16 15:57:56 +0000

    Remove warning for arcade recipes

---
+++ src/main/java/net/casual/arcade/mixin/recipes/ServerRecipeBookMixin.java
@@ -0,0 +1,24 @@
+package net.casual.arcade.mixin.recipes;
+
+import com.llamalad7.mixinextras.injector.WrapWithCondition;
+import net.casual.arcade.Arcade;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.stats.ServerRecipeBook;
+import org.slf4j.Logger;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(ServerRecipeBook.class)
+public class ServerRecipeBookMixin {
+	@WrapWithCondition(
+		method = "loadRecipes",
+		at = @At(
+			value = "INVOKE",
+			target = "Lorg/slf4j/Logger;error(Ljava/lang/String;Ljava/lang/Object;)V",
+			ordinal = 0
+		)
+	)
+	private boolean onNoRecipeExists(Logger instance, String string, Object o) {
+		return !Arcade.MOD_ID.equals(((ResourceLocation) o).getNamespace());
+	}
+}
