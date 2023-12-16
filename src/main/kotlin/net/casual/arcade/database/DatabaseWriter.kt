a05fcd4be28a71f29790ed4d8c1b436518857484
senseiwells <wellsm488@gmail.com>
2023-12-16 12:18:19 +0000

    Add createSpecific number elements

--- src/main/kotlin/net/casual/arcade/database/DatabaseWriter.kt
+++ src/main/kotlin/net/casual/arcade/database/DatabaseWriter.kt
@@ -7,9 +7,21 @@ public interface DatabaseWriter<E> {
 
     public fun createStringElement(string: String): DatabaseElement<E>
 
-    public fun createNumberElement(number: Number): DatabaseElement<E>
+    public fun createDoubleElement(double: Double): DatabaseElement<E>
+
+    public fun createLongElement(long: Long): DatabaseElement<E>
+
+    public fun createIntegerElement(integer: Int): DatabaseElement<E>
 
     public fun createBooleanElement(boolean: Boolean): DatabaseElement<E>
 
     public fun createEmptyElement(): DatabaseElement<E>
+
+    public fun createNumberElement(number: Number): DatabaseElement<E> {
+        return when (number) {
+            is Int -> this.createIntegerElement(number)
+            is Long -> this.createLongElement(number)
+            else -> this.createDoubleElement(number.toDouble())
+        }
+    }
 }
