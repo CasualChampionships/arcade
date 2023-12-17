e9272243f59b154ebd6f658c1a4d23a32e57409a
senseiwells <wellsm488@gmail.com>
2023-12-17 18:30:33 +0000

    Add utility methods in SidebarComponent

--- src/main/kotlin/net/casual/arcade/gui/sidebar/SidebarComponent.kt
+++ src/main/kotlin/net/casual/arcade/gui/sidebar/SidebarComponent.kt
@@ -1,9 +1,21 @@
 package net.casual.arcade.gui.sidebar
 
 import net.minecraft.network.chat.Component
+import net.minecraft.network.chat.numbers.BlankFormat
+import net.minecraft.network.chat.numbers.FixedFormat
 import net.minecraft.network.chat.numbers.NumberFormat
 
 public data class SidebarComponent(
     val display: Component,
     val score: NumberFormat? = null
-)
+) {
+    public companion object {
+        public fun withNoScore(component: Component): SidebarComponent {
+            return SidebarComponent(component, BlankFormat.INSTANCE)
+        }
+
+        public fun withCustomScore(component: Component, score: Component): SidebarComponent {
+            return SidebarComponent(component, FixedFormat(score))
+        }
+    }
+}
