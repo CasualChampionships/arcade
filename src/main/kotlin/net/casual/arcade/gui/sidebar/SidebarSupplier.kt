f119ef2d450a43143aed202cf66fa5ad38adb58e
senseiwells <wellsm488@gmail.com>
2023-12-17 18:46:45 +0000

    More util

--- src/main/kotlin/net/casual/arcade/gui/sidebar/SidebarSupplier.kt
+++ src/main/kotlin/net/casual/arcade/gui/sidebar/SidebarSupplier.kt
@@ -1,13 +1,26 @@
 package net.casual.arcade.gui.sidebar
 
+import net.minecraft.network.chat.Component
 import net.minecraft.server.level.ServerPlayer
 
 public fun interface SidebarSupplier {
     public fun getComponent(player: ServerPlayer): SidebarComponent
 
     public companion object {
+        public fun empty(): SidebarSupplier {
+            return ofNoScore(Component.empty())
+        }
+
         public fun of(component: SidebarComponent): SidebarSupplier {
             return Constant(component)
+        }
+
+        public fun ofNoScore(component: Component): SidebarSupplier {
+            return of(SidebarComponent.withNoScore(component))
+        }
+
+        public fun ofCustomScore(component: Component, score: Component): SidebarSupplier {
+            return of(SidebarComponent.withCustomScore(component, score))
         }
     }
 
