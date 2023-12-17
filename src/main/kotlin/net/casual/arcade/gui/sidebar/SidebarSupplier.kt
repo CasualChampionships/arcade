4f43dbfe2005729e5d4af5e3524f2493d2e32aec
senseiwells <wellsm488@gmail.com>
2023-12-17 18:33:00 +0000

    Add utility methods to SidebarSupplier

--- src/main/kotlin/net/casual/arcade/gui/sidebar/SidebarSupplier.kt
+++ src/main/kotlin/net/casual/arcade/gui/sidebar/SidebarSupplier.kt
@@ -4,4 +4,16 @@ import net.minecraft.server.level.ServerPlayer
 
 public fun interface SidebarSupplier {
     public fun getComponent(player: ServerPlayer): SidebarComponent
+
+    public companion object {
+        public fun of(component: SidebarComponent): SidebarSupplier {
+            return Constant(component)
+        }
+    }
+
+    private class Constant(private val component: SidebarComponent): SidebarSupplier {
+        override fun getComponent(player: ServerPlayer): SidebarComponent {
+            return this.component
+        }
+    }
 }
