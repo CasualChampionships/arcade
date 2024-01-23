751fe2fcd6fdbf891fe161f198abadfce212144f
senseiwells <wellsm488@gmail.com>
2024-01-23 12:21:53 +0000

    Add more loottable utils

--- src/main/kotlin/net/casual/arcade/utils/LootTableUtils.kt
+++ src/main/kotlin/net/casual/arcade/utils/LootTableUtils.kt
@@ -1,7 +1,11 @@
 package net.casual.arcade.utils
 
+import net.minecraft.world.level.ItemLike
 import net.minecraft.world.level.storage.loot.LootPool
 import net.minecraft.world.level.storage.loot.LootTable
+import net.minecraft.world.level.storage.loot.entries.LootItem
+import net.minecraft.world.level.storage.loot.entries.LootPoolEntryContainer
+import net.minecraft.world.level.storage.loot.entries.LootPoolSingletonContainer
 
 public object LootTableUtils {
     public fun create(applier: LootTable.Builder.() -> Unit): LootTable {
@@ -14,5 +18,20 @@ public object LootTableUtils {
         val builder = LootPool.lootPool()
         applier(builder)
         this.withPool(builder)
+    }
+
+    public fun <T: LootPoolEntryContainer.Builder<*>> LootPool.Builder.add(
+        element: T,
+        applier: T.() -> Unit
+    ) {
+        applier(element)
+        this.add(element)
+    }
+
+    public fun LootPool.Builder.addItem(
+        item: ItemLike,
+        applier: LootPoolSingletonContainer.Builder<*>.() -> Unit
+    ) {
+        this.add(LootItem.lootTableItem(item), applier)
     }
 }
