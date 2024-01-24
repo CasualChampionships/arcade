0e6492bf5a622855847c3df510b167400d6581b7
senseiwells <wellsm488@gmail.com>
2024-01-24 10:53:58 +0000

    Add more loot utils

--- src/main/kotlin/net/casual/arcade/utils/LootTableUtils.kt
+++ src/main/kotlin/net/casual/arcade/utils/LootTableUtils.kt
@@ -6,8 +6,23 @@ import net.minecraft.world.level.storage.loot.LootTable
 import net.minecraft.world.level.storage.loot.entries.LootItem
 import net.minecraft.world.level.storage.loot.entries.LootPoolEntryContainer
 import net.minecraft.world.level.storage.loot.entries.LootPoolSingletonContainer
+import net.minecraft.world.level.storage.loot.functions.EnchantRandomlyFunction
+import net.minecraft.world.level.storage.loot.functions.EnchantWithLevelsFunction
+import net.minecraft.world.level.storage.loot.functions.SetItemCountFunction
+import net.minecraft.world.level.storage.loot.functions.SetItemDamageFunction
+import net.minecraft.world.level.storage.loot.providers.number.ConstantValue
+import net.minecraft.world.level.storage.loot.providers.number.NumberProvider
+import net.minecraft.world.level.storage.loot.providers.number.UniformGenerator
 
 public object LootTableUtils {
+    public fun exactly(value: Number): ConstantValue {
+        return ConstantValue.exactly(value.toFloat())
+    }
+
+    public fun between(min: Number, max: Number): UniformGenerator {
+        return UniformGenerator.between(min.toFloat(), max.toFloat())
+    }
+
     public fun create(applier: LootTable.Builder.() -> Unit): LootTable {
         val builder = LootTable.lootTable()
         applier(builder)
@@ -33,5 +48,21 @@ public object LootTableUtils {
         applier: LootPoolSingletonContainer.Builder<*>.() -> Unit
     ) {
         this.add(LootItem.lootTableItem(item), applier)
+    }
+
+    public fun LootPoolSingletonContainer.Builder<*>.count(provider: NumberProvider) {
+        this.apply(SetItemCountFunction.setCount(provider))
+    }
+
+    public fun LootPoolSingletonContainer.Builder<*>.durability(provider: NumberProvider) {
+        this.apply(SetItemDamageFunction.setDamage(provider))
+    }
+
+    public fun LootPoolSingletonContainer.Builder<*>.enchant() {
+        this.apply(EnchantRandomlyFunction.randomApplicableEnchantment())
+    }
+
+    public fun LootPoolSingletonContainer.Builder<*>.enchant(levels: NumberProvider) {
+        this.apply(EnchantWithLevelsFunction.enchantWithLevels(levels))
     }
 }
