c018743c131bcbe2baeee9eab056828166bdd710
senseiwells <wellsm488@gmail.com>
2024-02-06 16:31:59 +0000

    Fix player ticking

--- src/main/java/net/casual/arcade/mixin/minigame/ServerGamePacketListenerImplMixin.java
+++ src/main/java/net/casual/arcade/mixin/minigame/ServerGamePacketListenerImplMixin.java
@@ -2,6 +2,10 @@ package net.casual.arcade.mixin.minigame;
 
 import net.casual.arcade.utils.MinigameUtils;
 import net.minecraft.network.Connection;
+import net.minecraft.network.protocol.game.ServerboundMovePlayerPacket;
+import net.minecraft.network.protocol.game.ServerboundMoveVehiclePacket;
+import net.minecraft.network.protocol.game.ServerboundUseItemOnPacket;
+import net.minecraft.network.protocol.game.ServerboundUseItemPacket;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.server.network.CommonListenerCookie;
@@ -29,6 +33,66 @@ public abstract class ServerGamePacketListenerImplMixin extends ServerCommonPack
 	private void onTick(CallbackInfo ci) {
 		if (!MinigameUtils.isTicking(this.player)) {
 			this.keepConnectionAlive();
+			ci.cancel();
+		}
+	}
+
+	@Inject(
+		method = "handleUseItem",
+		at = @At(
+			value = "INVOKE",
+			target = "Lnet/minecraft/network/protocol/PacketUtils;ensureRunningOnSameThread(Lnet/minecraft/network/protocol/Packet;Lnet/minecraft/network/PacketListener;Lnet/minecraft/server/level/ServerLevel;)V",
+			shift = At.Shift.AFTER
+		),
+		cancellable = true
+	)
+	private void onUseItem(ServerboundUseItemPacket packet, CallbackInfo ci) {
+		if (!MinigameUtils.isTicking(this.player)) {
+			ci.cancel();
+		}
+	}
+
+	@Inject(
+		method = "handleUseItemOn",
+		at = @At(
+			value = "INVOKE",
+			target = "Lnet/minecraft/network/protocol/PacketUtils;ensureRunningOnSameThread(Lnet/minecraft/network/protocol/Packet;Lnet/minecraft/network/PacketListener;Lnet/minecraft/server/level/ServerLevel;)V",
+			shift = At.Shift.AFTER
+		),
+		cancellable = true
+	)
+	private void onUseItemOn(ServerboundUseItemOnPacket packet, CallbackInfo ci) {
+		if (!MinigameUtils.isTicking(this.player)) {
+			ci.cancel();
+		}
+	}
+
+	@Inject(
+		method = "handleMovePlayer",
+		at = @At(
+			value = "INVOKE",
+			target = "Lnet/minecraft/network/protocol/PacketUtils;ensureRunningOnSameThread(Lnet/minecraft/network/protocol/Packet;Lnet/minecraft/network/PacketListener;Lnet/minecraft/server/level/ServerLevel;)V",
+			shift = At.Shift.AFTER
+		),
+		cancellable = true
+	)
+	private void onHandleMovement(ServerboundMovePlayerPacket packet, CallbackInfo ci) {
+		if (!MinigameUtils.isTicking(this.player)) {
+			ci.cancel();
+		}
+	}
+
+	@Inject(
+		method = "handleMoveVehicle",
+		at = @At(
+			value = "INVOKE",
+			target = "Lnet/minecraft/network/protocol/PacketUtils;ensureRunningOnSameThread(Lnet/minecraft/network/protocol/Packet;Lnet/minecraft/network/PacketListener;Lnet/minecraft/server/level/ServerLevel;)V",
+			shift = At.Shift.AFTER
+		),
+		cancellable = true
+	)
+	private void onHandleVehicleMovement(ServerboundMoveVehiclePacket packet, CallbackInfo ci) {
+		if (!MinigameUtils.isTicking(this.player)) {
 			ci.cancel();
 		}
 	}
