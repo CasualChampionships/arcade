1fe3286944cc72869ae4dc25eda516c7f2882968
senseiwells <wellsm488@gmail.com>
2024-02-06 17:21:27 +0000

    Fix some more synching issues

--- src/main/java/net/casual/arcade/mixin/minigame/ServerGamePacketListenerImplMixin.java
+++ src/main/java/net/casual/arcade/mixin/minigame/ServerGamePacketListenerImplMixin.java
@@ -2,15 +2,14 @@ package net.casual.arcade.mixin.minigame;
 
 import net.casual.arcade.utils.MinigameUtils;
 import net.minecraft.network.Connection;
-import net.minecraft.network.protocol.game.ServerboundMovePlayerPacket;
-import net.minecraft.network.protocol.game.ServerboundMoveVehiclePacket;
-import net.minecraft.network.protocol.game.ServerboundUseItemOnPacket;
-import net.minecraft.network.protocol.game.ServerboundUseItemPacket;
+import net.minecraft.network.protocol.game.*;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.server.network.CommonListenerCookie;
 import net.minecraft.server.network.ServerCommonPacketListenerImpl;
 import net.minecraft.server.network.ServerGamePacketListenerImpl;
+import net.minecraft.world.entity.Entity;
+import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -21,13 +20,20 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public abstract class ServerGamePacketListenerImplMixin extends ServerCommonPacketListenerImpl {
 	@Shadow public ServerPlayer player;
 
+	@Shadow public abstract void teleport(double x, double y, double z, float yaw, float pitch);
+
+	@Shadow private @Nullable Entity lastVehicle;
+
 	public ServerGamePacketListenerImplMixin(MinecraftServer server, Connection connection, CommonListenerCookie cookie) {
 		super(server, connection, cookie);
 	}
 
 	@Inject(
 		method = "tick",
-		at = @At("HEAD"),
+		at = @At(
+			value = "INVOKE",
+			target = "Lnet/minecraft/server/network/ServerGamePacketListenerImpl;resetPosition()V"
+		),
 		cancellable = true
 	)
 	private void onTick(CallbackInfo ci) {
@@ -41,13 +47,14 @@ public abstract class ServerGamePacketListenerImplMixin extends ServerCommonPack
 		method = "handleUseItem",
 		at = @At(
 			value = "INVOKE",
-			target = "Lnet/minecraft/network/protocol/PacketUtils;ensureRunningOnSameThread(Lnet/minecraft/network/protocol/Packet;Lnet/minecraft/network/PacketListener;Lnet/minecraft/server/level/ServerLevel;)V",
+			target = "Lnet/minecraft/server/network/ServerGamePacketListenerImpl;ackBlockChangesUpTo(I)V",
 			shift = At.Shift.AFTER
 		),
 		cancellable = true
 	)
 	private void onUseItem(ServerboundUseItemPacket packet, CallbackInfo ci) {
 		if (!MinigameUtils.isTicking(this.player)) {
+			this.player.inventoryMenu.sendAllDataToRemote();
 			ci.cancel();
 		}
 	}
@@ -56,13 +63,14 @@ public abstract class ServerGamePacketListenerImplMixin extends ServerCommonPack
 		method = "handleUseItemOn",
 		at = @At(
 			value = "INVOKE",
-			target = "Lnet/minecraft/network/protocol/PacketUtils;ensureRunningOnSameThread(Lnet/minecraft/network/protocol/Packet;Lnet/minecraft/network/PacketListener;Lnet/minecraft/server/level/ServerLevel;)V",
+			target = "Lnet/minecraft/server/network/ServerGamePacketListenerImpl;ackBlockChangesUpTo(I)V",
 			shift = At.Shift.AFTER
 		),
 		cancellable = true
 	)
 	private void onUseItemOn(ServerboundUseItemOnPacket packet, CallbackInfo ci) {
 		if (!MinigameUtils.isTicking(this.player)) {
+			this.player.inventoryMenu.sendAllDataToRemote();
 			ci.cancel();
 		}
 	}
@@ -78,6 +86,13 @@ public abstract class ServerGamePacketListenerImplMixin extends ServerCommonPack
 	)
 	private void onHandleMovement(ServerboundMovePlayerPacket packet, CallbackInfo ci) {
 		if (!MinigameUtils.isTicking(this.player)) {
+			this.teleport(
+				this.player.getX(),
+				this.player.getY(),
+				this.player.getZ(),
+				this.player.getYRot(),
+				this.player.getXRot()
+			);
 			ci.cancel();
 		}
 	}
@@ -93,6 +108,26 @@ public abstract class ServerGamePacketListenerImplMixin extends ServerCommonPack
 	)
 	private void onHandleVehicleMovement(ServerboundMoveVehiclePacket packet, CallbackInfo ci) {
 		if (!MinigameUtils.isTicking(this.player)) {
+			ci.cancel();
+			Entity entity = this.player.getRootVehicle();
+			if (entity != this.player && entity.getControllingPassenger() == this.player && entity == this.lastVehicle) {
+				this.send(new ClientboundMoveVehiclePacket(entity));
+			}
+		}
+	}
+
+	@Inject(
+		method = "handleContainerClick",
+		at = @At(
+			value = "INVOKE",
+			target = "Lnet/minecraft/network/protocol/PacketUtils;ensureRunningOnSameThread(Lnet/minecraft/network/protocol/Packet;Lnet/minecraft/network/PacketListener;Lnet/minecraft/server/level/ServerLevel;)V",
+			shift = At.Shift.AFTER
+		),
+		cancellable = true
+	)
+	private void onHandleClick(ServerboundContainerClickPacket packet, CallbackInfo ci) {
+		if (!MinigameUtils.isTicking(this.player)) {
+			this.player.inventoryMenu.sendAllDataToRemote();
 			ci.cancel();
 		}
 	}
