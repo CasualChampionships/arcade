a701421fc64b757baf7b514b16836adeebd5895b
senseiwells <wellsm488@gmail.com>
2024-02-15 00:34:15 +0000

    Add Direction8.opposite

--- src/main/kotlin/net/casual/arcade/utils/MathUtils.kt
+++ src/main/kotlin/net/casual/arcade/utils/MathUtils.kt
@@ -1,5 +1,8 @@
 package net.casual.arcade.utils
 
+import net.minecraft.core.Direction
+import net.minecraft.core.Direction8
+import net.minecraft.world.entity.Entity
 import net.minecraft.world.phys.Vec3
 import kotlin.math.ceil
 import kotlin.math.floor
@@ -15,6 +18,19 @@ public object MathUtils {
 
     public operator fun Vec3.component3(): Double {
         return this.z
+    }
+
+    public fun Direction8.opposite(): Direction8 {
+        return when (this) {
+            Direction8.NORTH -> Direction8.SOUTH
+            Direction8.NORTH_EAST -> Direction8.SOUTH_WEST
+            Direction8.EAST -> Direction8.WEST
+            Direction8.SOUTH_EAST -> Direction8.NORTH_WEST
+            Direction8.SOUTH -> Direction8.NORTH
+            Direction8.SOUTH_WEST -> Direction8.NORTH_EAST
+            Direction8.WEST -> Direction8.EAST
+            Direction8.NORTH_WEST -> Direction8.SOUTH_EAST
+        }
     }
 
     public fun Double.wholeOrNull(): Int? {
