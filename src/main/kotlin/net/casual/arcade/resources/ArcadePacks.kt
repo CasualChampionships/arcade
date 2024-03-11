b7bb2796ffa27feedc4d88fdbc2a89494a4a503d
senseiwells <wellsm488@gmail.com>
2024-03-11 22:58:26 +0000

    Add pack descriptions

--- src/main/kotlin/net/casual/arcade/resources/ArcadePacks.kt
+++ src/main/kotlin/net/casual/arcade/resources/ArcadePacks.kt
@@ -1,6 +1,7 @@
 package net.casual.arcade.resources
 
 import net.casual.arcade.Arcade
+import net.casual.arcade.utils.ComponentUtils.literal
 
 /**
  * Contains some commonly used resource packs.
@@ -14,12 +15,15 @@ public object ArcadePacks {
     init {
         ACTION_BAR_FONT_PACK = NamedResourcePackCreator.named("action_bar_font") {
             addAssetSource(Arcade.container.findPath("packs/ActionBarFont").get())
+            packDescription = "Resource pack which provides fonts to shift text on the action bar".literal()
         }
         SPACES_FONT_PACK = NamedResourcePackCreator.named("spaces_font") {
             addAssetSource(Arcade.container.findPath("packs/SpacesFont").get())
+            packDescription = "Resource pack which provides spacing utilities for text".literal()
         }
         NO_SHADOW_PACK = NamedResourcePackCreator.named("no_shadow") {
             addAssetSource(Arcade.container.findPath("packs/NoShadow").get())
+            packDescription = "Resource pack which provides utilities for removing text shadows".literal()
         }
     }
 }
