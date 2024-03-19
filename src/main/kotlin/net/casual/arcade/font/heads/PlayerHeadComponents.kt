f20d1b635c96e406603897ea694c19ba35276324
senseiwells <wellsm488@gmail.com>
2024-03-19 18:10:33 +0000

    Async

--- src/main/kotlin/net/casual/arcade/font/heads/PlayerHeadComponents.kt
+++ src/main/kotlin/net/casual/arcade/font/heads/PlayerHeadComponents.kt
@@ -1,6 +1,7 @@
 package net.casual.arcade.font.heads
 
 import dev.fruxz.kojang.Kojang
+import kotlinx.coroutines.future.future
 import kotlinx.coroutines.runBlocking
 import net.casual.arcade.Arcade
 import net.casual.arcade.utils.ComponentUtils
@@ -12,104 +13,102 @@ import java.io.IOException
 import java.net.URL
 import java.util.*
 import java.util.concurrent.CompletableFuture
+import java.util.concurrent.ConcurrentHashMap
 import javax.imageio.ImageIO
 
 public object PlayerHeadComponents {
-    private val futures = HashMap<UUID, CompletableFuture<Component>>()
-    private val uuidCache = HashMap<UUID, Component?>()
-    private val namedCache = HashMap<String, Component?>()
+    private val cache = ConcurrentHashMap<UUID, CompletableFuture<Component>>()
+    private val nameToUUIDCache = ConcurrentHashMap<String, UUID>()
+    private val invalidNames = ConcurrentHashMap.newKeySet<String>()
 
-    public fun getHeadFor(player: ServerPlayer): Component {
-        val uuid = player.uuid
-        if (this.uuidCache.containsKey(uuid)) {
-            return this.uuidCache[uuid] ?: PlayerHeadFont.STEVE_HEAD
-        }
-        return this.generateHead(player.scoreboardName, player.uuid, true) {
-            player.server.sessionService.getTextures(player.gameProfile).skin?.url
-        }
+    private val default = CompletableFuture.completedFuture<Component>(PlayerHeadFont.STEVE_HEAD)
+
+    public fun getHeadOrDefault(player: ServerPlayer): Component {
+        return this.getHead(player).getNow(PlayerHeadFont.STEVE_HEAD)
     }
 
-    public fun getHeadFor(name: String): Component {
-        if (this.namedCache.containsKey(name)) {
-            return this.namedCache[name] ?: PlayerHeadFont.STEVE_HEAD
+    public fun getHeadOrDefault(name: String): Component {
+        return this.getHead(name).getNow(PlayerHeadFont.STEVE_HEAD)
+    }
+
+    public fun getHead(player: ServerPlayer): CompletableFuture<Component> {
+        val uuid = player.uuid
+        if (this.cache.containsKey(uuid)) {
+            return this.cache[uuid]!!
         }
+        val skinUrl = player.server.sessionService.getTextures(player.gameProfile).skin?.url ?: return this.default
+        val future = CompletableFuture.supplyAsync {
+            this.generateHead(skinUrl)
+        }
+        this.cache[uuid] = future
+        return future
+    }
+
+    public fun getHead(name: String): CompletableFuture<Component> {
+        if (this.invalidNames.contains(name)) {
+            return this.default
+        }
+
         val player = PlayerUtils.player(name)
         if (player != null) {
-            return this.getHeadFor(player)
+            // This is faster since we don't have to look up uuid
+            return this.getHead(player)
         }
-        return runBlocking {
-            val profile = Kojang.getMojangUserProfile(name)
+        val cachedUUID = this.nameToUUIDCache[name]
+        if (cachedUUID != null) {
+            val cached = this.cache[cachedUUID]
+            if (cached != null) {
+                return cached
+            }
+        }
+
+        // I would use co-routines for everything, but I want to
+        // make this library Java friendly.
+        // I think it's more appropriate to make everything futures
+        var future: CompletableFuture<Component>? = null
+        future = runBlocking {
+            future {
+                Kojang.getMojangUserProfile(name)
+            }
+        }.thenApply { profile ->
             if (profile == null) {
+                this.invalidNames.add(name)
                 PlayerHeadFont.STEVE_HEAD
             } else {
-                generateHead(profile.username, UUID.fromString(profile.uuid), true) {
-                    profile.textures.skin.url
+                val uuid = UUID.fromString(profile.uuid)
+                this.nameToUUIDCache[name] = uuid
+                val cached = this.cache[uuid]
+                if (cached != null) {
+                    cached.join()
+                } else {
+                    this.cache[uuid] = future!!
+                    val component = this.generateHead(profile.textures.skin.url)
+                    component
                 }
             }
         }
+        return future
     }
 
-    private fun generateHead(
-        name: String,
-        uuid: UUID,
-        block: Boolean,
-        supplier: () -> String?
-    ): Component {
-        val future = this.futures[uuid]
-        if (future == null) {
-            val url = supplier()
-            if (url == null) {
-                this.uuidCache[uuid] = null
-                this.namedCache[name] = null
-            } else {
-                val newFuture = this.generateHeadAsync(url)
-                this.futures[uuid] = newFuture
-                if (block) {
-                    return newFuture.join()
+    private fun generateHead(skinTextureUrl: String): Component {
+        try {
+            val image = ImageIO.read(URL(skinTextureUrl))
+            val component = Component.empty()
+            for (y in 0..7) {
+                for (x in 0..7) {
+                    if (x != 0) {
+                        component.append(ComponentUtils.space(-1))
+                    }
+                    component.append(PlayerHeadFont.pixel(y).colour(image.getRGB(x + 8, y + 8)))
+                }
+                if (y != 7) {
+                    component.append(ComponentUtils.space(-9))
                 }
             }
+            return component
+        } catch (e: IOException) {
+            Arcade.logger.error("Failed to generate head texture from url: $skinTextureUrl", e)
             return PlayerHeadFont.STEVE_HEAD
-        }
-
-        if (!future.isDone) {
-            if (block) {
-                return future.join()
-            }
-            return PlayerHeadFont.STEVE_HEAD
-        }
-        this.futures.remove(uuid)
-        if (future.isCompletedExceptionally || future.isCancelled) {
-            this.uuidCache[uuid] = null
-            this.namedCache[name] = null
-            return PlayerHeadFont.STEVE_HEAD
-        }
-        val component = future.get()
-        this.uuidCache[uuid] = component
-        this.namedCache[name] = component
-        return component
-    }
-
-    private fun generateHeadAsync(skinTextureUrl: String): CompletableFuture<Component> {
-        return CompletableFuture.supplyAsync {
-            try {
-                val image = ImageIO.read(URL(skinTextureUrl))
-                val component = Component.empty()
-                for (y in 0..7) {
-                    for (x in 0..7) {
-                        if (x != 0) {
-                            component.append(ComponentUtils.space(-1))
-                        }
-                        component.append(PlayerHeadFont.pixel(y).colour(image.getRGB(x + 8, y + 8)))
-                    }
-                    if (y != 7) {
-                        component.append(ComponentUtils.space(-9))
-                    }
-                }
-                component
-            } catch (e: IOException) {
-                Arcade.logger.error("Failed to generate head texture from url: $skinTextureUrl", e)
-                PlayerHeadFont.STEVE_HEAD
-            }
         }
     }
 }
