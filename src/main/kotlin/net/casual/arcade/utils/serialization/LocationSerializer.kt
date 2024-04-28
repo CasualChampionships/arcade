c84f98997e02033912ceef2d18dd7a4a1321045f
senseiwells <wellsm488@gmail.com>
2024-04-28 01:56:32 +0100

    Fix number

--- src/main/kotlin/net/casual/arcade/utils/serialization/LocationSerializer.kt
+++ src/main/kotlin/net/casual/arcade/utils/serialization/LocationSerializer.kt
@@ -10,14 +10,14 @@ import net.casual.arcade.minigame.events.lobby.LocationConfig
 
 public object LocationSerializer: KSerializer<LocationConfig> {
     @OptIn(ExperimentalSerializationApi::class)
-    override val descriptor: SerialDescriptor = SerialDescriptor("Location", serializer<Map<String, Number>>().descriptor)
+    override val descriptor: SerialDescriptor = SerialDescriptor("Location", serializer<Map<String, Double>>().descriptor)
 
     override fun deserialize(decoder: Decoder): LocationConfig {
-        val map = decoder.decodeSerializableValue(serializer<Map<String, Number>>())
+        val map = decoder.decodeSerializableValue(serializer<Map<String, Double>>())
         return LocationConfig(
-            map.getOrDefault("x", 0.0).toDouble(),
-            map.getOrDefault("y", 0.0).toDouble(),
-            map.getOrDefault("z", 0.0).toDouble(),
+            map.getOrDefault("x", 0.0),
+            map.getOrDefault("y", 0.0),
+            map.getOrDefault("z", 0.0),
             map.getOrDefault("yaw", 0.0).toFloat(),
             map.getOrDefault("pitch", 0.0).toFloat()
         )
@@ -28,9 +28,9 @@ public object LocationSerializer: KSerializer<LocationConfig> {
             "x" to value.x,
             "y" to value.y,
             "z" to value.z,
-            "yaw" to value.yaw,
-            "pitch" to value.pitch
+            "yaw" to value.yaw.toDouble(),
+            "pitch" to value.pitch.toDouble()
         )
-        encoder.encodeSerializableValue(serializer<Map<String, Number>>(), map)
+        encoder.encodeSerializableValue(serializer<Map<String, Double>>(), map)
     }
 }
