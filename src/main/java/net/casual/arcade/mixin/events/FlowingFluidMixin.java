5f0c1db98134f0fa34398f65e49bccf994201ae2
senseiwells <wellsm488@gmail.com>
2024-06-01 14:37:32 +0100

    Add LevelFluidTrySpreadEvent

---
+++ src/main/java/net/casual/arcade/mixin/events/FlowingFluidMixin.java
@@ -0,0 +1,53 @@
+package net.casual.arcade.mixin.events;
+
+import com.llamalad7.mixinextras.injector.ModifyReturnValue;
+import net.casual.arcade.events.GlobalEventHandler;
+import net.casual.arcade.events.level.LevelFluidTrySpreadEvent;
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.material.FlowingFluid;
+import net.minecraft.world.level.material.Fluid;
+import net.minecraft.world.level.material.FluidState;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+@Mixin(FlowingFluid.class)
+public class FlowingFluidMixin {
+	@ModifyReturnValue(
+		method = "canSpreadTo",
+		at = @At("RETURN")
+	)
+	private boolean onCanSpreadTo(
+		boolean original,
+		BlockGetter level,
+		BlockPos fromPos,
+		BlockState fromBlockState,
+		Direction direction,
+		BlockPos toPos,
+		BlockState toBlockState,
+		FluidState toFluidState,
+		Fluid fluid
+	) {
+		if (level instanceof ServerLevel serverLevel) {
+			LevelFluidTrySpreadEvent event = new LevelFluidTrySpreadEvent(
+				serverLevel,
+				fromPos,
+				fromBlockState,
+				direction,
+				toPos,
+				toBlockState,
+				toFluidState,
+				fluid,
+				original
+			);
+			GlobalEventHandler.broadcast(event);
+			if (event.isCancelled()) {
+				return event.result();
+			}
+		}
+		return original;
+	}
+}
