849fa73c26a73f06b52ea59552d134065c3a0381
senseiwells <wellsm488@gmail.com>
2024-06-19 17:48:50 +0100

    Remove arcade mixins

--- datagen/build.gradle.kts
+++ datagen/build.gradle.kts
@@ -27,7 +27,7 @@ publishing {
         create<MavenPublication>("arcadeDatagen") {
             groupId = "com.github.CasualChampionships"
             artifactId = "arcade-datagen"
-            version = "1.0.13"
+            version = "1.0.14"
             from(components["java"])
         }
     }
