de27e9c1ff4517561fef29a8b0974ecb01d103e0
senseiwells <wellsm488@gmail.com>
2024-06-23 11:39:28 +0100

    Remove move event

--- src/main/kotlin/net/casual/arcade/events/entity/EntityMoveEvent.kt
+++
@@ -1,9 +0,0 @@
-package net.casual.arcade.events.entity
-
-import net.minecraft.world.entity.Entity
-import net.minecraft.world.phys.Vec3
-
-public data class EntityMoveEvent(
-    override val entity: Entity,
-    val position: Vec3
-): EntityEvent
--- src/main/kotlin/net/casual/arcade/events/player/PlayerMoveEvent.kt
+++
@@ -1,9 +0,0 @@
-package net.casual.arcade.events.player
-
-import net.minecraft.server.level.ServerPlayer
-import net.minecraft.world.phys.Vec3
-
-public data class PlayerMoveEvent(
-    override val player: ServerPlayer,
-    val position: Vec3
-): PlayerEvent
