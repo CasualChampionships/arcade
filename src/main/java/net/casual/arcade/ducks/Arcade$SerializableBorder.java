b8f484bb7ba4032ba907e7cf96b239734bd2c9e5
senseiwells <wellsm488@gmail.com>
2024-06-23 22:23:35 +0100

    Fix serializable border

--- src/main/java/net/casual/arcade/ducks/Arcade$SerializableBorder.java
+++ src/main/java/net/casual/arcade/ducks/Arcade$SerializableBorder.java
@@ -1,25 +1,10 @@
 package net.casual.arcade.ducks;
 
-import net.casual.arcade.border.SerializableBorder;
 import net.minecraft.nbt.CompoundTag;
-import org.jetbrains.annotations.ApiStatus.NonExtendable;
 import org.jetbrains.annotations.NotNull;
 
-public interface Arcade$SerializableBorder extends SerializableBorder {
+public interface Arcade$SerializableBorder {
 	CompoundTag arcade$serialize();
 
 	void arcade$deserialize(@NotNull CompoundTag compound);
-
-	@NotNull
-	@Override
-	@NonExtendable
-	default CompoundTag serialize() {
-		return this.arcade$serialize();
-	}
-
-	@Override
-	@NonExtendable
-	default void deserialize(@NotNull CompoundTag compound) {
-		this.arcade$deserialize(compound);
-	}
 }
