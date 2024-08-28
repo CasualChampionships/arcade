dcdf77e8bb563092e5eedd1bae962812d428a385
senseiwells <wellsm488@gmail.com>
2024-08-28 12:21:22 +0100

    Update FantasyUtils

--- src/main/kotlin/net/casual/arcade/utils/FantasyUtils.kt
+++ src/main/kotlin/net/casual/arcade/utils/FantasyUtils.kt
@@ -9,6 +9,7 @@ import net.casual.arcade.level.VanillaLikeRuntimeLevel
 import net.casual.arcade.mixin.fantasy.FantasyInvoker
 import net.minecraft.resources.ResourceLocation
 import net.minecraft.server.MinecraftServer
+import net.minecraft.server.level.ServerLevel
 import net.minecraft.world.level.dimension.BuiltinDimensionTypes
 import xyz.nucleoid.fantasy.Fantasy
 import xyz.nucleoid.fantasy.RuntimeWorldConfig
@@ -17,10 +18,20 @@ import xyz.nucleoid.fantasy.RuntimeWorldHandle
 public object FantasyUtils {
     public fun createTemporaryVanillaLikeLevels(
         server: MinecraftServer = Arcade.getServer(),
+        seed: Long
+    ): VanillaLikeLevels {
+        val overworldConfig = this.createOverworldConfig(seed)
+        val netherConfig = this.createNetherConfig(seed)
+        val endConfig = this.createEndConfig(seed)
+        return this.createTemporaryVanillaLikeLevels(server, overworldConfig, netherConfig, endConfig)
+    }
+
+    public fun createTemporaryVanillaLikeLevels(
+        server: MinecraftServer = Arcade.getServer(),
         overworldConfig: RuntimeWorldConfig,
         netherConfig: RuntimeWorldConfig,
         endConfig: RuntimeWorldConfig
-    ): Triple<RuntimeWorldHandle, RuntimeWorldHandle, RuntimeWorldHandle> {
+    ): VanillaLikeLevels {
         val fantasy = Fantasy.get(server)
         val dimensions = this.setWorldConstructors(overworldConfig, netherConfig, endConfig)
 
@@ -29,24 +40,22 @@ public object FantasyUtils {
         val end = fantasy.openTemporaryWorld(endConfig)
 
         this.setOtherDimensions(dimensions, overworld, nether, end)
-        return Triple(overworld, nether, end)
+        return VanillaLikeLevels(overworld, nether, end)
     }
 
-    public fun createNullableTemporaryVanillaLikeLevels(
+    public fun createPersistentVanillaLikeLevels(
         server: MinecraftServer = Arcade.getServer(),
-        overworldConfig: RuntimeWorldConfig? = null,
-        netherConfig: RuntimeWorldConfig? = null,
-        endConfig: RuntimeWorldConfig? = null
-    ): Triple<RuntimeWorldHandle?, RuntimeWorldHandle?, RuntimeWorldHandle?> {
-        val fantasy = Fantasy.get(server)
-        val dimensions = this.setWorldConstructors(overworldConfig, netherConfig, endConfig)
-
-        val overworld = overworldConfig?.let { fantasy.openTemporaryWorld(it) }
-        val nether = netherConfig?.let { fantasy.openTemporaryWorld(it) }
-        val end = endConfig?.let { fantasy.openTemporaryWorld(it) }
-
-        this.setOtherDimensions(dimensions, overworld, nether, end)
-        return Triple(overworld, nether, end)
+        overworldId: ResourceLocation,
+        netherId: ResourceLocation,
+        endId: ResourceLocation,
+        seed: Long
+    ): VanillaLikeLevels {
+        return this.createPersistentVanillaLikeLevels(
+            server,
+            PersistentConfig(overworldId, this.createOverworldConfig(seed)),
+            PersistentConfig(netherId, this.createNetherConfig(seed)),
+            PersistentConfig(endId, this.createEndConfig(seed))
+        )
     }
 
     public fun createPersistentVanillaLikeLevels(
@@ -54,7 +63,7 @@ public object FantasyUtils {
         overworldConfig: PersistentConfig,
         netherConfig: PersistentConfig,
         endConfig: PersistentConfig
-    ): Triple<RuntimeWorldHandle, RuntimeWorldHandle, RuntimeWorldHandle> {
+    ): VanillaLikeLevels {
         val fantasy = Fantasy.get(server)
         val dimensions = this.setWorldConstructors(overworldConfig.config, netherConfig.config, endConfig.config)
 
@@ -63,24 +72,7 @@ public object FantasyUtils {
         val end = fantasy.getOrOpenPersistentWorld(endConfig.location, endConfig.config)
 
         this.setOtherDimensions(dimensions, overworld, nether, end)
-        return Triple(overworld, nether, end)
-    }
-
-    public fun createNullablePersistentVanillaLikeLevels(
-        server: MinecraftServer = Arcade.getServer(),
-        overworldConfig: PersistentConfig? = null,
-        netherConfig: PersistentConfig? = null,
-        endConfig: PersistentConfig? = null
-    ): Triple<RuntimeWorldHandle?, RuntimeWorldHandle?, RuntimeWorldHandle?> {
-        val fantasy = Fantasy.get(server)
-        val dimensions = this.setWorldConstructors(overworldConfig?.config, netherConfig?.config, endConfig?.config)
-
-        val overworld = overworldConfig?.let { fantasy.getOrOpenPersistentWorld(it.location, it.config) }
-        val nether = netherConfig?.let { fantasy.getOrOpenPersistentWorld(it.location, it.config) }
-        val end = endConfig?.let { fantasy.getOrOpenPersistentWorld(it.location, it.config) }
-
-        this.setOtherDimensions(dimensions, overworld, nether, end)
-        return Triple(overworld, nether, end)
+        return VanillaLikeLevels(overworld, nether, end)
     }
 
     public fun createOverworldConfig(seed: Long = 0L): RuntimeWorldConfig {
@@ -106,32 +98,49 @@ public object FantasyUtils {
     }
 
     private fun setWorldConstructors(
-        overworld: RuntimeWorldConfig? = null,
-        nether: RuntimeWorldConfig? = null,
-        end: RuntimeWorldConfig? = null
+        overworld: RuntimeWorldConfig,
+        nether: RuntimeWorldConfig,
+        end: RuntimeWorldConfig
     ): VanillaLikeDimensions {
         val others = VanillaLikeDimensions()
-        overworld?.setWorldConstructor(VanillaLikeRuntimeLevel.constructor(Overworld, others))
-        nether?.setWorldConstructor(VanillaLikeRuntimeLevel.constructor(Nether, others))
-        end?.setWorldConstructor(VanillaLikeRuntimeLevel.constructor(End, others))
+        overworld.setWorldConstructor(VanillaLikeRuntimeLevel.constructor(Overworld, others))
+        nether.setWorldConstructor(VanillaLikeRuntimeLevel.constructor(Nether, others))
+        end.setWorldConstructor(VanillaLikeRuntimeLevel.constructor(End, others))
         return others
     }
 
     private fun setOtherDimensions(
         dimensions: VanillaLikeDimensions,
-        overworld: RuntimeWorldHandle?,
-        nether: RuntimeWorldHandle?,
-        end: RuntimeWorldHandle?
+        overworld: RuntimeWorldHandle,
+        nether: RuntimeWorldHandle,
+        end: RuntimeWorldHandle
     ) {
-        dimensions.overworld = overworld?.registryKey
-        dimensions.nether = nether?.registryKey
-        dimensions.end = end?.registryKey
+        dimensions.overworld = overworld.registryKey
+        dimensions.nether = nether.registryKey
+        dimensions.end = end.registryKey
     }
 
     public class PersistentConfig(
         public val location: ResourceLocation,
         public val config: RuntimeWorldConfig
     )
+
+    public data class VanillaLikeLevels(
+        val overworldHandle: RuntimeWorldHandle,
+        val netherHandle: RuntimeWorldHandle,
+        val endHandle: RuntimeWorldHandle
+    ) {
+        val overworld: ServerLevel
+            get() = this.overworldHandle.asWorld()
+        val nether: ServerLevel
+            get() = this.netherHandle.asWorld()
+        val end: ServerLevel
+            get() = this.endHandle.asWorld()
+
+        public fun levels(): Triple<ServerLevel, ServerLevel, ServerLevel> {
+            return Triple(this.overworld, this.nether, this.end)
+        }
+    }
 
     internal fun registerEvents() {
         GlobalEventHandler.register<ServerStoppingEvent>(priority = Int.MAX_VALUE) {
