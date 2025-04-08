6b4ad56de3178341d51e4f0f9260a1b2f52a369b
senseiwells <wellsm488@gmail.com>
2025-04-08 20:45:02 +0100

    feat: Add optional filter to RegistryElementArgument

--- arcade-commands/src/main/kotlin/net/casual/arcade/commands/arguments/RegistryElementArgument.kt
+++ arcade-commands/src/main/kotlin/net/casual/arcade/commands/arguments/RegistryElementArgument.kt
@@ -15,16 +15,21 @@ import net.casual.arcade.commands.type.CustomArgumentTypeInfo
 import net.minecraft.commands.SharedSuggestionProvider
 import net.minecraft.commands.arguments.ResourceLocationArgument
 import net.minecraft.core.Registry
+import net.minecraft.core.RegistryAccess
 import net.minecraft.network.chat.Component
 import net.minecraft.resources.ResourceKey
 import net.minecraft.resources.ResourceLocation
 import java.util.concurrent.CompletableFuture
 
 public class RegistryElementArgument<T>(
-    private val key: ResourceKey<Registry<T>>
-): CustomArgumentType<ResourceKey<T>>() {
-    override fun parse(reader: StringReader): ResourceKey<T> {
-        return ResourceKey.create(this.key, ResourceLocation.read(reader))
+    private val key: ResourceKey<Registry<T>>,
+    private val filter: (ResourceKey<T>, T) -> Boolean
+): CustomArgumentType<RegistryElementArgument.FilterableResourceKey<T>>() {
+    override fun parse(reader: StringReader): FilterableResourceKey<T> {
+        return FilterableResourceKey(
+            ResourceKey.create(this.key, ResourceLocation.read(reader)),
+            this.filter
+        )
     }
 
     override fun <S> listSuggestions(
@@ -36,7 +41,11 @@ public class RegistryElementArgument<T>(
             val optional = source.registryAccess().lookup(this.key)
             if (optional.isPresent) {
                 val registry = optional.get()
-                return SharedSuggestionProvider.suggestResource(registry.keySet(), builder)
+                return SharedSuggestionProvider.suggestResource(
+                    registry.entrySet().filter { (key, value) ->
+                        this.filter.invoke(key, value)
+                    }.map { it.key.location() }, builder
+                )
             }
         }
         return super.listSuggestions(context, builder)
@@ -46,28 +55,41 @@ public class RegistryElementArgument<T>(
         return CustomArgumentTypeInfo.of(ResourceLocationArgument::class.java)
     }
 
+    public data class FilterableResourceKey<T>(
+        private val key: ResourceKey<T>,
+        private val filter: (ResourceKey<T>, T) -> Boolean
+    ) {
+        public fun getElement(access: RegistryAccess): T {
+            val registry = access.lookup(this.key.registryKey())
+            if (registry.isEmpty) {
+                throw UNKNOWN_REGISTRY.create(this.key.registryKey().location())
+            }
+            val value = registry.get().getOptional(this.key)
+            if (value.isEmpty || !this.filter.invoke(this.key, value.get())) {
+                throw INVALID_ELEMENT.create(this.key.location(), this.key.registryKey().location())
+            }
+            return value.get()
+        }
+    }
+
     public companion object {
         private val UNKNOWN_REGISTRY = DynamicCommandExceptionType { Component.translatable("commands.arguments.registry.unknown", it) }
         private val INVALID_ELEMENT = Dynamic2CommandExceptionType { a, b -> Component.translatable("commands.arguments.registry.element.unknown", a, b) }
 
         @JvmStatic
-        public fun <T> element(key: ResourceKey<Registry<T>>): RegistryElementArgument<T> {
-            return RegistryElementArgument(key)
+        @JvmOverloads
+        public fun <T> element(
+            key: ResourceKey<Registry<T>>,
+            filter: (ResourceKey<T>, T) -> Boolean = { _, _ -> true }
+        ): RegistryElementArgument<T> {
+            return RegistryElementArgument(key, filter)
         }
 
         @JvmStatic
         public fun <T> getElement(context: CommandContext<out SharedSuggestionProvider>, string: String): T {
             @Suppress("UNCHECKED_CAST")
-            val key = context.getArgument(string, ResourceKey::class.java) as ResourceKey<T>
-            val registry = context.source.registryAccess().lookup(key.registryKey())
-            if (registry.isEmpty) {
-                throw UNKNOWN_REGISTRY.create(key.registryKey().location())
-            }
-            val value = registry.get().getOptional(key)
-            if (value.isEmpty) {
-                throw INVALID_ELEMENT.create(key.location(), key.registryKey().location())
-            }
-            return value.get()
+            val filterable = context.getArgument(string, FilterableResourceKey::class.java) as FilterableResourceKey<T>
+            return filterable.getElement(context.source.registryAccess())
         }
     }
 }
