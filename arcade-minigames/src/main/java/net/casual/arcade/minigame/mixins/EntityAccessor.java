644c71220a7bfccd4e259f370162595ce2acb23d
senseiwells <wellsm488@gmail.com>
2025-05-29 12:38:21 +0100

    chore: Spotless

--- arcade-minigames/src/main/java/net/casual/arcade/minigame/mixins/EntityAccessor.java
+++ arcade-minigames/src/main/java/net/casual/arcade/minigame/mixins/EntityAccessor.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (c) 2025 senseiwells
+ * Licensed under the MIT License. See LICENSE file in the project root for details.
+ */
 package net.casual.arcade.minigame.mixins;
 
 import net.minecraft.world.entity.Entity;
