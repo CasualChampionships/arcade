83cd5f681608b0bd374aa41431a7e49ffea81eab
senseiwells <wellsm488@gmail.com>
2025-08-31 11:24:56 +0100

    feat: Implement CodecSerializersModule

--- README.md
+++ README.md
@@ -36,7 +36,7 @@ repositories {
 }
 
 dependencies {
-    include(modImplementation("net.casualchampionships:arcade:0.6.1-beta.11+1.21.8")!!)
+    include(modImplementation("net.casualchampionships:arcade:0.6.1-beta.12+1.21.8")!!)
 }
 ```
 
---
+++ arcade-utils/src/main/kotlin/net/casual/arcade/utils/serialization/kotlin/KotlinSerializationUtils.kt
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2025 senseiwells
+ * Licensed under the MIT License. See LICENSE file in the project root for details.
+ */
+package net.casual.arcade.utils.serialization.kotlin
+
+import com.mojang.serialization.Codec
+import com.mojang.serialization.DynamicOps
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.json.JsonBuilder
+import kotlinx.serialization.json.JsonElement
+import kotlinx.serialization.modules.SerializersModule
+import kotlinx.serialization.modules.SerializersModuleBuilder
+import kotlinx.serialization.modules.SerializersModuleCollector
+import net.minecraft.core.HolderLookup
+import kotlin.reflect.KClass
+
+@Suppress("UnusedReceiverParameter", "FunctionName")
+public fun JsonBuilder.CodecSerializersModule(
+    lookup: HolderLookup.Provider,
+    block: CodecSerializersModuleBuilder.() -> Unit
+): SerializersModule {
+    return SerializersModule {
+        val builder = CodecSerializersModuleBuilder(this, lookup.createSerializationContext(KJsonOps))
+        builder.block()
+    }
+}
+
+@Suppress("UnusedReceiverParameter", "FunctionName")
+public fun JsonBuilder.CodecSerializersModule(
+    block: CodecSerializersModuleBuilder.() -> Unit
+): SerializersModule {
+    return SerializersModule {
+        val builder = CodecSerializersModuleBuilder(this, KJsonOps)
+        builder.block()
+    }
+}
+
+@OptIn(ExperimentalSerializationApi::class)
+public class CodecSerializersModuleBuilder(
+    private val inner: SerializersModuleBuilder,
+    private val ops: DynamicOps<JsonElement>
+): SerializersModuleCollector by inner {
+    public inline fun <reified T: Any> contextual(codec: Codec<T>) {
+        this.contextual(T::class, codec)
+    }
+
+    public fun <T: Any> contextual(clazz: KClass<T>, codec: Codec<T>) {
+        this.inner.contextual(clazz, KJsonCodecSerializer(codec, this.ops))
+    }
+}
