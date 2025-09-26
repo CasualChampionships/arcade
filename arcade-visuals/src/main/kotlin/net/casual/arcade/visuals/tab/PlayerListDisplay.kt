5a059856a907a465b0987a8af8f57e949ef0a1ec
senseiwells <wellsm488@gmail.com>
2025-09-26 21:50:37 +0100

    chore: Add the ability to specify spectator format to PlayerListEntries.Entry

--- arcade-visuals/src/main/kotlin/net/casual/arcade/visuals/tab/PlayerListDisplay.kt
+++ arcade-visuals/src/main/kotlin/net/casual/arcade/visuals/tab/PlayerListDisplay.kt
@@ -197,7 +197,7 @@ public open class PlayerListDisplay(
                 null,
                 true,
                 entry.latency,
-                GameType.SURVIVAL,
+                if (entry.spectatorFormat) GameType.SPECTATOR else GameType.SURVIVAL,
                 entry.display,
                 entry.showHat,
                 -index, // For some reason, Mojang does this in reverse
@@ -222,7 +222,7 @@ public open class PlayerListDisplay(
             profile,
             true,
             entry.latency,
-            GameType.SURVIVAL,
+            if (entry.spectatorFormat) GameType.SPECTATOR else GameType.SURVIVAL,
             entry.display,
             entry.showHat,
             -index,
--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
     java
 }
 
-val modVersion = "0.6.2-beta.47"
+val modVersion = "0.6.2-beta.48"
 
 allprojects {
     apply(plugin = "org.jetbrains.kotlin.jvm")
