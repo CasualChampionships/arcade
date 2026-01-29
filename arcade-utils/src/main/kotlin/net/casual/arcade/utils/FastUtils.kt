f7e106434daedf2fa5a4f959f6f435b602354ec2
senseiwells <wellsm488@gmail.com>
2026-01-29 17:38:50 +0000

    feat: Implement EasingUtils

--- arcade-utils/src/main/kotlin/net/casual/arcade/utils/FastUtils.kt
+++ arcade-utils/src/main/kotlin/net/casual/arcade/utils/FastUtils.kt
@@ -5,6 +5,9 @@
 package net.casual.arcade.utils
 
 import it.unimi.dsi.fastutil.Pair
+import it.unimi.dsi.fastutil.doubles.DoubleDoublePair
+import it.unimi.dsi.fastutil.floats.FloatFloatPair
+import it.unimi.dsi.fastutil.ints.IntIntPair
 
 public operator fun <A: Any, B> Pair<A, B>.component1(): A {
     return this.left()
@@ -12,4 +15,28 @@ public operator fun <A: Any, B> Pair<A, B>.component1(): A {
 
 public operator fun <A: Any, B> Pair<A, B>.component2(): B {
     return this.right()
+}
+
+public operator fun IntIntPair.component1(): Int {
+    return this.leftInt()
+}
+
+public operator fun IntIntPair.component2(): Int {
+    return this.rightInt()
+}
+
+public operator fun FloatFloatPair.component1(): Float {
+    return this.leftFloat()
+}
+
+public operator fun FloatFloatPair.component2(): Float {
+    return this.rightFloat()
+}
+
+public operator fun DoubleDoublePair.component1(): Double {
+    return this.leftDouble()
+}
+
+public operator fun DoubleDoublePair.component2(): Double {
+    return this.rightDouble()
 }
