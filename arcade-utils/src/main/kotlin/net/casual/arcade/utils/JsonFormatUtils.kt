d6f379a9013289d70f0b2c14a2b69a2acbc82bc4
senseiwells <wellsm488@gmail.com>
2026-02-06 23:37:56 +0000

    feat: Implement JsonFormatUtils

---
+++ arcade-utils/src/main/kotlin/net/casual/arcade/utils/JsonFormatUtils.kt
@@ -0,0 +1,23 @@
+package net.casual.arcade.utils
+
+public object JsonFormatUtils {
+    private const val NUMBER = "-?\\d+(?:\\.\\d+)?(?:[eE][+-]?\\d+)?"
+
+    private val COMPACT_ARRAY_2_3 = Regex(
+        "\\[\\s*($NUMBER)\\s*,\\s*($NUMBER)\\s*(?:,\\s*($NUMBER)\\s*)?]"
+    )
+
+    public fun compactSmallNumericArrays(json: String): String {
+        return COMPACT_ARRAY_2_3.replace(json) { match ->
+            val a = match.groupValues[1]
+            val b = match.groupValues[2]
+            val c = match.groupValues.getOrNull(3)
+
+            if (!c.isNullOrEmpty()) {
+                "[$a, $b, $c]"
+            } else {
+                "[$a, $b]"
+            }
+        }
+    }
+}
