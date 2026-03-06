5182c194a453232d09da3fe2260b5ccf7dc72f63
senseiwells <wellsm488@gmail.com>
2026-03-06 15:45:40 +0000

    fix: AdventureSpectator ticks frozen desync

---
+++ arcade-minigames/src/main/java/net/casual/arcade/minigame/mixins/gamemode/InsideBlockEffectTypeMixin.java
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2026 senseiwells
+ * Licensed under the MIT License. See LICENSE file in the project root for details.
+ */
+package net.casual.arcade.minigame.mixins.gamemode;
+
+import net.casual.arcade.minigame.gamemode.ExtendedGameMode;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.InsideBlockEffectType;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import static net.casual.arcade.minigame.gamemode.ExtendedGameMode.getExtendedGameMode;
+
+@Mixin(InsideBlockEffectType.class)
+public class InsideBlockEffectTypeMixin {
+    @Inject(
+        method = "method_67647",
+        at = @At("TAIL")
+    )
+    private static void updateAdventureSpectatorFrozenTicks(Entity entity, CallbackInfo ci) {
+        // The client doesn't know that it can't be frozen since it thinks it's in
+        // adventure mode so the ticks frozen counter gets desynced
+        if (entity instanceof ServerPlayer player && getExtendedGameMode(player) == ExtendedGameMode.AdventureSpectator) {
+            // Mark the entity data dirty
+            player.setTicksFrozen(1);
+            player.clearFreeze();
+        }
+    }
+}
--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
     java
 }
 
-val modVersion = "0.8.1-beta.42"
+val modVersion = "0.8.1-beta.43"
 
 allprojects {
     apply(plugin = "org.jetbrains.kotlin.jvm")
