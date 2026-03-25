1d8763beace7dda3bdbf5ac48952c4214a7a0bbd
senseiwells <wellsm488@gmail.com>
2026-03-25 20:23:29 +0000

    chore: Set testmod rundir

--- build.gradle.kts
+++ build.gradle.kts
@@ -150,6 +150,7 @@ loom {
             server()
             source(testmod)
             vmArgs("-Dmixin.debug.export=true")
+            runDir = "run/${libs.versions.minecraft.get()}"
         }
     }
     createRemapConfigurations(testmod)
