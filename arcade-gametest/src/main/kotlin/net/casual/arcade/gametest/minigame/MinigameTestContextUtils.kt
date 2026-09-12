4c2b5e9bd854cae0dff4326dd035015f07cda54c
senseiwells <wellsm488@gmail.com>
2026-09-12 17:18:40 +0100

    fix: reloading test minigame taking variable amount of ticks

--- arcade-gametest/src/main/kotlin/net/casual/arcade/gametest/minigame/MinigameTestContextUtils.kt
+++ arcade-gametest/src/main/kotlin/net/casual/arcade/gametest/minigame/MinigameTestContextUtils.kt
@@ -9,6 +9,7 @@ import net.casual.arcade.minigame.Minigame
 import net.casual.arcade.minigame.Minigames
 import net.casual.arcade.minigame.serialization.SerializableMinigame
 import net.casual.arcade.minigame.serialization.save
+import net.casual.arcade.utils.coroutine.joinBlocking
 import net.minecraft.server.MinecraftServer
 import net.minecraft.world.level.storage.LevelResource
 import java.nio.file.Path
@@ -17,6 +18,7 @@ import kotlin.io.path.ExperimentalPathApi
 import kotlin.io.path.copyToRecursively
 import kotlin.io.path.createParentDirectories
 import kotlin.io.path.deleteRecursively
+import kotlin.time.Duration.Companion.seconds
 
 public fun <M: Minigame> TestContext.minigame(
     constructor: (MinecraftServer, UUID) -> M
@@ -28,7 +30,7 @@ public fun TestContext.track(minigame: Minigame) {
     this.track(AutoCloseable(minigame::close))
 }
 
-public suspend fun <M> TestContext.reload(minigame: M): M where M: Minigame, M: SerializableMinigame {
+public fun <M> TestContext.reload(minigame: M): M where M: Minigame, M: SerializableMinigame {
     val copy = saveCopy(minigame)
     minigame.close()
 
@@ -53,7 +55,7 @@ public fun TestContext.copySave(minigame: Minigame): Path {
     return copy
 }
 
-private suspend fun <M> TestContext.saveCopy(minigame: M): Path where M: Minigame, M: SerializableMinigame {
-    minigame.save().join()
+private fun <M> TestContext.saveCopy(minigame: M): Path where M: Minigame, M: SerializableMinigame {
+    minigame.save().joinBlocking(1.seconds)
     return this.copySave(minigame)
 }
--- arcade-utils/src/main/kotlin/net/casual/arcade/utils/coroutine/CoroutineUtils.kt
+++ arcade-utils/src/main/kotlin/net/casual/arcade/utils/coroutine/CoroutineUtils.kt
@@ -6,7 +6,34 @@ package net.casual.arcade.utils.coroutine
 
 import kotlinx.coroutines.Deferred
 import kotlinx.coroutines.ExperimentalCoroutinesApi
-import net.casual.arcade.utils.coroutine.getNowOrNull
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.withTimeout
+import kotlin.time.Duration
+
+public fun Job.joinBlocking() {
+    val job = this
+    runBlocking { job.join() }
+}
+
+public fun Job.joinBlocking(timeout: Duration) {
+    val job = this
+    runBlocking {
+        withTimeout(timeout) { job.join() }
+    }
+}
+
+public fun <T> Deferred<T>.awaitBlocking(): T {
+    val deferred = this
+    return runBlocking { deferred.await() }
+}
+
+public fun <T> Deferred<T>.awaitBlocking(timeout: Duration): T {
+    val deferred = this
+    return runBlocking {
+        withTimeout(timeout) { deferred.await() }
+    }
+}
 
 public fun <T> Deferred<T>.getNow(default: T & Any): T & Any {
     return this.getNowOrNull() ?: default
